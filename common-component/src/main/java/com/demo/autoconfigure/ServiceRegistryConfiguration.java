41151980e2e56b8d1961b4299a28103e1749fe96
Sridhar Balasubramanian <Sridhar Balasubramanian>
2017-06-08 01:01:33 +0530

    'changes'

--- common-component/src/main/java/com/demo/autoconfigure/ServiceRegistryConfiguration.java
+++ common-component/src/main/java/com/demo/autoconfigure/ServiceRegistryConfiguration.java
@@ -3,8 +3,11 @@ package com.demo.autoconfigure;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 
 @Profile("REGISTRY")
+@AutoConfigureBefore(EurekaClientAutoConfiguration.class)
 @Configuration
 @EnableEurekaClient
 public class ServiceRegistryConfiguration {
